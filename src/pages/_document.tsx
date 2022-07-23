1a9d8b0de0c6c0ba33be5f93cbe93e7aa2ebb3d9
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2022-07-23 13:22:52 -0300

    Adicionado atributo lang no HTML

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,13 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="pt_BR">
       <Head>
         <meta name="description" content="Comparamos as principais seguradoras de seguro viagem do Brasil, os melhores planos de seguro viagem e preços do mercado. Cobrimos qualquer oferta. Confira!" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+        
       </Head>
       <body>
         <Main />
